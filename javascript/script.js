3761329a85e2eb44420e1817b762f819348b83ea
ingridflatjord <54808948+ingridflatjord@users.noreply.github.com>
2019-11-04 18:31:05 +0100

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,18 @@
+// Automatisk slideshow av 5 ulike bilder på forsiden av nettsiden. Hverrt bilde vises i 5 sek.
+let images = 0
+
+function slideshow() {
+  let count
+  let slides = document.getElementsByClassName("front_images")
+  for (let count = 0; count < slides.length; count++) {
+    slides[count].style.display = "none"
+  }
+
+  images++
+  if (images > slides.length) {images = 1}
+  slides[images-1].style.display = "block"
+
+  setTimeout(slideshow, 5000)
+}
+
+slideshow()
