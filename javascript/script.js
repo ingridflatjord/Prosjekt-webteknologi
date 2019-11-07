aae88ef77c39c54abefbe4ef355d20bfb7f0cafc
Fisken94 <56640753+Fisken94@users.noreply.github.com>
2019-11-07 11:53:53 +0100

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -16,3 +16,52 @@ function slideshow() {
 }
 
 slideshow()
+
+/* Meny */
+
+
+/* Kjøttretter */
+// Set variables for key elements
+var menuDiv = document.getElementById('menuDiv');
+var menuToggle = document.getElementById('menuToggle');
+
+// Start by adding the class "collapse" to the menuDiv
+menuDiv.classList.add('collapsed');
+
+// Establish a function to toggle the class "collapse"
+function mainmenuToggle() {
+    menuDiv.classList.toggle('collapsed');
+}
+
+
+// Add a click event to run the mainmenuToggle function
+menuToggle.addEventListener('click', mainmenuToggle);
+
+/* Vegetar */
+// Set variables for key elements
+var menuDiv1 = document.getElementById('menuDiv1');
+var menuToggle1 = document.getElementById('menuToggle1');
+
+// Start by adding the class "collapse" to the menuDiv
+menuDiv1.classList.add('collapsed');
+
+// Establish a function to toggle the class "collapse"
+function mainmenuToggle1() {
+    menuDiv1.classList.toggle('collapsed');
+}
+
+/* Drikke */
+// Set variables for key elements
+var menuDiv2 = document.getElementById('menuDiv2');
+var menuToggle2 = document.getElementById('menuToggle2');
+
+// Start by adding the class "collapse" to the menuDiv
+menuDiv2.classList.add('collapsed');
+
+// Establish a function to toggle the class "collapse"
+function mainmenuToggle2() {
+    menuDiv2.classList.toggle('collapsed');
+}
+
+
+
