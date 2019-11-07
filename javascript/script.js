7be3cabab4dd2b857769a2172a35111855e365f0
ingridflatjord <54808948+ingridflatjord@users.noreply.github.com>
2019-11-07 13:26:26 +0100

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,28 @@
+//Topbaren endrer seg når skjermstørrelsen endres.
+const navSlide = () => {
+  const burger = document.querySelector(".burger")
+  const nav = document.querySelector(".nav-links")
+  const navLinks = document.querySelectorAll(".nav-links li")
+
+  burger.addEventListener("click", () => {
+      //Toggle now
+      nav.classList.toggle("nav-active")
+
+      //Adding a nice delay between each choise
+      navLinks.forEach((link, index) => {
+        if (link.style.animation) {
+          link.style.animation = ""
+        }
+        else {
+          link.style.animation = "navLinkFade 0.5s ease forwards ${index / 7 + 1.3}s"
+        }
+      })
+    })
+
+}
+
+navSlide()
+
 // Automatisk slideshow av 5 ulike bilder på forsiden av nettsiden. Hvert bilde vises i 5 sek.
 let images = 0
 
