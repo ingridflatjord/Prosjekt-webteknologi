d54e7c547eb64ff3e207e387d855cb5f66295ef7
jasminae <54809931+jasminae@users.noreply.github.com>
2019-11-07 14:41:44 +0100

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,25 +1,3 @@
-//Topbaren endrer seg når skjermstørrelsen endres.
-const navSlide = () => {
-  const burger = document.querySelector(".burger")
-  const nav = document.querySelector(".nav-links")
-  const navLinks = document.querySelectorAll(".nav-links li")
-
-  burger.addEventListener("click", () => {
-      nav.classList.toggle("nav-active")
-
-      navLinks.forEach((link, index) => {
-        if (link.style.animation) {
-          link.style.animation = ""
-        }
-        else {
-          link.style.animation = "navLinkFade 0.5s ease forwards ${index / 7 + 1.3}s"
-        }
-      })
-    })
-
-}
-
-navSlide()
 // Automatisk slideshow av 5 ulike bilder på forsiden av nettsiden. Hvert bilde vises i 5 sek.
 let images = 0
 
