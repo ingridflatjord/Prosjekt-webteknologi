4c58f6ade331c0d2829126d3f04fabc1eff5eb87
ingridflatjord <54808948+ingridflatjord@users.noreply.github.com>
2019-11-09 00:41:11 +0100

    Update header_footer.js

--- javascript/header_footer.js
+++ javascript/header_footer.js
@@ -1,22 +1,35 @@
-const header = document.getElementById('header');
+// Naviagtion bar
+const header = document.getElementById('header')
 function lagNav() {
   const headerElementer=
-  '<nav>'+
-    '<div class="topnav">'+
-      '<ul class="main-topbar">'+
-        '<li><a href="menu.html"> Meny </a></li>'+
-        '<li><a href="openinghours.html"> Åpningstider </a></li>'+
-        '<li><img src="../images/logo.png" class="logo" alt="logo"></li>'+
-        '<li><a href="information.html"> Informasjon </a></li>'+
-        '<li><a href="frontpage.html"> Forside </a></li>'+
-      '</ul>'+
-    '</div>'+
-  '</nav>';
+  `<div class ="main_navbar" id="navigationbar">
+      <a href=""><img src="../images/logo.png" class="logo" alt="logo"></a>
+      <ul>
+        <li><a href="menu.html"> Meny </a></li>
+        <li><a href="openinghours.html"> Åpningstider </a></li>
+        <li><a href="information.html"> Informasjon </a></li>
+        <li><a href="frontpage.html"> Forside </a></li>
+      </ul>
+      <a class="burger" href="javascript:void(0)" onclick="changeNavbar()">&#9776;</a>
+  </div>`
 
-  return headerElementer;
+  return headerElementer
 }
 
 header.innerHTML = lagNav();
+
+//When the burger are pressed, a drop-down menu will show.
+function changeNavbar() {
+  const header = document.getElementById("navigationbar")
+  console.log("heiii");
+
+  if (header.className === "main_navbar") {
+    header.className += " responsive"
+  } else {
+    header.className = "main_navbar"
+  }
+}
+
 
 
 const footer = document.getElementById('footer')
