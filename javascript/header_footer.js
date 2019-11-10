c890fbb0269d58c07a161496e74c9b42d9f975fc
ingridflatjord <54808948+ingridflatjord@users.noreply.github.com>
2019-11-10 01:02:45 +0100

    Update header_footer.js

--- javascript/header_footer.js
+++ javascript/header_footer.js
@@ -33,40 +33,30 @@ const footer = document.getElementById('footer')
 
 function lagFooter() {
     const footerElementer =
-
-    '<div id="kontakt">'+
-    '<div>'+
-    '<form class="buttons" action="https://www.instagram.com/cafesaldo/">' +
-        '<input id="instagram" type="image" src="../images/instagram.png" alt="Instagram">' +
-    '</form>' +
-
-    '<form class="buttons" action="https://www.facebook.com/">' +
-      '<input id="facebook" type="image" src="../images/facebook.png" alt="Facebook">' +
-    '</form>' +
-    '</div>'+
-
-      '<p><b><u>Kontakt</u></b></p>'+
-      '<p id="tlf"> Tlf: <a href="tel:97969924">97969924</a></p>'+
-      '<p id="mail"> Mail: <a href="mailto:cafesaldotrondheim@gmail.com">cafesaldotrondheim@gmail.com</a></p>'+
-    '</div>'+
-
-
-    '<div>'+
-
-    '<ul id="apningstider_footer" style="list-style:none;">'+
-      '<li><b><u>Åpningstider</u></b></li>'+
-      '<li>Mandag - Fredag: <b>08.30 - 17.00</b></li>'+
-      '<li>Lørdag: <b>10.00 - 15.00</b></li>'+
-      '<li>Søndag: <b>Stengt</b></li>'+
-    '</ul>'+
-    '</div>'+
-
-    '</footer>'
+    `<form class="buttons" action="https://www.instagram.com/cafesaldo/">
+        <input id="instagram" type="image" src="../images/instagram.png" alt="Instagram">
+    </form>
+    <form class="buttons" action="https://www.facebook.com/">
+      <input id="facebook" type="image" src="../images/facebook.png" alt="Facebook">
+    </form>
+    <div id="kontakt">
+      <p><b><u>Kontakt</u></b></p>
+      <p id="tlf"> Tlf: <a href="tel:97969924">97969924</a></p>
+      <p id="mail"> Mail: <a href="mailto:cafesaldotrondheim@gmail.com">cafesaldotrondheim@gmail.com</a></p>
+    </div>
+    <ul id="apningstider_footer" style="list-style:none;">
+      <li><b><u>Åpningstider</u></b></li>
+      <li>Mandag - Fredag: <b>08.30 - 17.00</b></li>
+      <li>Lørdag: <b>10.00 - 15.00</b></li>
+      <li>Søndag: <b>Stengt</b></li>
+    </ul>
+    </footer>`
 
     return footerElementer
 }
 
 footer.innerHTML = lagFooter()
+
 
 /* Meny */
 const coursesK ={
